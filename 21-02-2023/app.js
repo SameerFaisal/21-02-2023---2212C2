5259eb896faa83538f1371b9271999d6ace6be5f
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-21 13:18:15 +0500

    21-02-2023 (2212C2)

---
+++ 21-02-2023/app.js
@@ -0,0 +1,126 @@
+// let num1=+prompt("Enter number 1")
+// let num2=+prompt("Enter number 2")
+
+// let add=num1+num2
+// let sub=num1-num2
+// let mul=num1*num2
+// let div=num1/num2
+
+// document.write(add)
+// document.write("<br>")
+// document.write(sub)
+// document.write("<br>")
+// document.write(mul)
+// document.write("<br>")
+// document.write(div)
+
+// alert("Hello World")
+// confirm("Are you sure?")
+
+// let firstNumber=prompt("Enter your first number") 
+// let secondNumber=+prompt("Enter second Number") 
+// let thirdNumber=+prompt("Enter third Number") 
+// let fourthNumber=prompt("Enter your fourth number")
+
+
+// let result=parseFloat(firstNumber)-secondNumber/thirdNumber+parseFloat(fourthNumber)
+// document.write(result) 
+// document.write(isNaN("two"))
+
+
+
+
+// num--   //Post Decrement    //Post-Fix operators
+// num++   //Post Increment
+
+// ++num    //Pre-Increment      //Pre-Fix Operators
+// --num    //Pre Decrement 
+// let num=2
+// document.write(num+"<br>") //2
+// document.write(num++)
+// document.write(++num)
+// document.write(--num)  //1
+// document.write("<br>")
+// document.write(num)  //1
+
+// let num=0
+// document.write(num++)  //0
+// document.write("<br>")
+// document.write(num--) //1
+// document.write("<br>")
+// document.write(num) //0
+// document.write("<br>")
+// document.write(--num) //-1
+// document.write("<br>")
+// document.write(++num) //0
+// document.write("<br>")
+// document.write(++num) //1
+// document.write("<br>")
+// document.write(++num) //2
+// document.write("<br>")
+// document.write(++num) //3
+// document.write("<br>")
+// document.write(num--)  //3
+// document.write("<br>")
+// document.write(num)  //2
+// document.write("<br>")
+// document.write(--num)  //1
+// document.write("<br>")
+// document.write(num--)  //1
+// document.write("<br>")
+// document.write(num)  //0
+// document.write("<br>")
+// document.write(--num)  //-1
+// document.write("<br>")
+// document.write(num--)  //-1
+// document.write("<br>")
+// document.write(num)  //-2
+// document.write("<br>")
+// document.write(--num)  //-3
+
+// let num=5
+// num++ 
+// num--  
+// ++num 
+// --num 
+// --num 
+// num-- 
+// document.write(num)
+
+
+// let num=5
+// document.write(num++) //5
+// document.write("<br>") 
+// document.write(num--)  //P6 S5
+// document.write("<br>")  
+// document.write(++num) //S6 P6
+// document.write("<br>") 
+// document.write(--num)  //S5   P5
+// document.write("<br>") 
+// document.write(--num)  //S4   P4
+// document.write("<br>") 
+// document.write(num--)  //P4   S3
+// document.write("<br>") 
+// document.write(num) //P3
+
+// document.write(2===2)
+// document.write(3!=="3")
+// document.write(5<=5)
+
+// document.write(5>3 && 2<5)
+
+// document.write(4>5 || 2<3)
+
+let price=+prompt("Enter price for an item")
+let percentage=+prompt("Enter discount percentage")
+
+let disc=price-(price*(percentage/100))
+document.write("Price after discount is: "+disc)
+
+
+
+
+
+
+
+
